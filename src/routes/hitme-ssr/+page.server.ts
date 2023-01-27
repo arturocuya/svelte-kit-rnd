e05c7f834b755e71fd1ebc6f322872c5c4996468
Arturo Cuya <arturocuya099@gmail.com>
2023-01-27 17:41:13 -0500

    add logs to ssr

--- src/routes/hitme-ssr/+page.server.ts
+++ src/routes/hitme-ssr/+page.server.ts
@@ -2,11 +2,15 @@ import type { AsyncReturnType } from "../../lib/asyncReturnType";
 import prisma from "../../lib/prisma";
 
 export async function load({ fetch }: any) {
+    console.log('hitme-ssr load() called');
     const endpoint = '/api/hitme';
     const res = await fetch(endpoint);
     const text = await res.text();
+    console.log('fetch finished');
 
+    console.log('starting prisma connection');
     await prisma.$connect();
+    console.log('prisma connection established');
 
     let users;
 
@@ -17,8 +21,10 @@ export async function load({ fetch }: any) {
         await prisma.$disconnect();
     }
 
+    console.log('found many, disconnecting')
     prisma.$disconnect();
 
+    console.log('hitme-ssr load() finished');
     return {
         message: text,
         users: users ?? []
