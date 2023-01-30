3d35db8febb48a707d18dabacc329637a6308276
Arturo Cuya <arturocuya099@gmail.com>
2023-01-29 19:41:34 -0500

    restore name page.server.ts

--- src/routes/hitme-ssr/+page.server.ts
+++ src/routes/hitme-ssr/+page.server.ts
@@ -1,39 +1,33 @@
+import type { User } from "@prisma/client";
 import prisma from "../../lib/prisma";
-import type { PageLoad } from "./$types";
+import type { PageServerLoad } from "./$types";
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ fetch }) => {
     console.log('hitme-ssr load() called');
     const endpoint = '/api/hitme';
     const res = await fetch(endpoint);
     const text = await res.text();
-    console.log('fetch finished');
 
-    console.log('starting prisma connection');
     try {
         await prisma.$connect();
     } catch(e) {
         console.log('connection error');
         console.log(e);
     }
-    
-    console.log('prisma connection established');
 
-    let users;
+    let users: User[] = [];
 
     try {
         users = await prisma.user.findMany();
     } catch(e) {
-        console.log('findMany error');
         console.log(e);
         await prisma.$disconnect();
     }
 
-    console.log('found many, disconnecting')
     prisma.$disconnect();
 
-    console.log('hitme-ssr load() finished');
     return {
         message: text,
-        users: users ?? [{ name: 'nobody', email: 'nobody@mail.com' }]
+        users: users
     }
 }
